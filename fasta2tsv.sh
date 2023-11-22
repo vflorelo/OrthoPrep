feec4bceddd59240cb459592fd213d9f2d662436
Victor Flores <vflorelo@gmail.com>
2023-11-22 14:46:45 +0000

    debugging

--- fasta2tsv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fasta_file=$1
-format_test=$(grep -c \> "${fasta_file}")
-if [ -z "${format_test}" ] || [ "${format_test}" -eq 0 ]
-then
-	echo "Not a fasta file"
-	exit 1
-fi
-perl -pe 'if(/\>/){s/$/\t/};s/\n//g;s/\>/\n\>/g' "${fasta_file}" | tail -n+2
