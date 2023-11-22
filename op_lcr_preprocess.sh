5e537a1022cfc8c274e341f9f6540866c0e0618a
Victor Flores <vflorelo@gmail.com>
2023-11-22 15:29:32 +0000

    debugging

--- op_lcr_preprocess.sh
+++ op_lcr_preprocess.sh
@@ -29,22 +29,19 @@ cd ${lcr_dir}
 for num in ${species_num}
 do
     echo ${num}
-    if [ -f "${prep_dir}/Species${num}.fa" ]
-    then       
-        cp "${prep_dir}/Species${num}.fa" "${lcr_dir}"
-        grep -w ^"${num}" "${sequence_len_file}" > "${sizes_tsv_file}"
-        CompositionMaker Species${num}.fa
-        perl -pe 'if(/\>/){s/$/\t/};s/\n//g;s/\>/\n\>/g' Species${num}.fa | tail -n+2 > Species${num}.seq.tsv
-        seq_id_list=$(cut -f1 "${sizes_tsv_file}" | sort -V | uniq)
-        parallel -j ${threads} run_flps ::: "${seq_id_list[@]}" ::: Species${num}.seq.tsv ::: Species${num}.fa.COMPOSITION | sort -V | uniq > Species${num}.flps.tsv 2> Species${num}.flps.err
-        awk 'BEGIN{FS="\t";OFS="\t"}{lcr_len=($6-($5-1))}{if(lcr_len>=10){print $1,$5-1,$6,$9,$8,"+"}}' Species${num}.flps.tsv | sortBed -i - > Species${num}.flps.bed
-        seg Species${num}.fa -l | grep \> | perl -pe 's/>//;s/ .*complexity=/\t/;s/ .*//;s/\(/\t/;s/\)//' | awk 'BEGIN{FS="\t";OFS="\t"}{gsub("-","\t",$2)}1' | awk 'BEGIN{FS="\t";OFS="\t"}{lcr_len=$3-($2-1)}{if(lcr_len>=10){print $1,$2-1,$3,"seg",$4,"+"}}' | sortBed -i - > Species${num}.seg.bed
-        intersectBed -a Species${num}.flps.bed -b Species${num}.seg.bed | sortBed -i - | mergeBed > "Species${num}.lcr.bed"
-        parallel -j ${threads} get_eff_len ::: "${seq_id_list[@]}" ::: "${sizes_tsv_file}" ::: "Species${num}.lcr.bed" | sort -V | uniq > "Species${num}.lcr.tsv"
-        if [ "${no_masking}" == "FALSE" ]
-        then
-            maskFastaFromBed -fi Species${num}.fa -bed Species${num}.lcr.bed -fo ${prep_dir}/Species${num}.fa -mc X
-        fi
+    cp "${prep_dir}/Species${num}.fa" "${lcr_dir}"
+    grep -w ^${num} ${sequence_len_file} > "${sizes_tsv_file}"
+    CompositionMaker Species${num}.fa
+    perl -pe 'if(/\>/){s/$/\t/};s/\n//g;s/\>/\n\>/g' Species${num}.fa | tail -n+2 > Species${num}.seq.tsv
+    seq_id_list=$(cut -f1 "${sizes_tsv_file}" | sort -V | uniq)
+    parallel -j ${threads} run_flps ::: "${seq_id_list[@]}" ::: Species${num}.seq.tsv ::: Species${num}.fa.COMPOSITION | sort -V | uniq > Species${num}.flps.tsv 2> Species${num}.flps.err
+    awk 'BEGIN{FS="\t";OFS="\t"}{lcr_len=($6-($5-1))}{if(lcr_len>=10){print $1,$5-1,$6,$9,$8,"+"}}' Species${num}.flps.tsv | sortBed -i - > Species${num}.flps.bed
+    seg Species${num}.fa -l | grep \> | perl -pe 's/>//;s/ .*complexity=/\t/;s/ .*//;s/\(/\t/;s/\)//' | awk 'BEGIN{FS="\t";OFS="\t"}{gsub("-","\t",$2)}1' | awk 'BEGIN{FS="\t";OFS="\t"}{lcr_len=$3-($2-1)}{if(lcr_len>=10){print $1,$2-1,$3,"seg",$4,"+"}}' | sortBed -i - > Species${num}.seg.bed
+    intersectBed -a Species${num}.flps.bed -b Species${num}.seg.bed | sortBed -i - | mergeBed > "Species${num}.lcr.bed"
+    parallel -j ${threads} get_eff_len ::: "${seq_id_list[@]}" ::: "${sizes_tsv_file}" ::: "Species${num}.lcr.bed" | sort -V | uniq > "Species${num}.lcr.tsv"
+    if [ "${no_masking}" == "FALSE" ]
+    then
+        maskFastaFromBed -fi Species${num}.fa -bed Species${num}.lcr.bed -fo ${prep_dir}/Species${num}.fa -mc X
     fi
 done
 cat Species*.lcr.tsv | awk 'BEGIN{FS="\t"}{print $1 "@" $1 FS $2}' | perl -pe 's/_.*\@/\t/' > "${sequence_len_file}"
