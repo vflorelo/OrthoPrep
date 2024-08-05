3f3d77ec8afc25d7ec6a980b5cf03be1aa8439ba
Victor Flores <vflorelo@merida.home>
2024-08-05 11:35:39 +0100

    added ancilliary scripts

--- get_og_stats.py
+++ get_og_stats.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import pandas as pd
 import statistics
 import sys
@@ -9,5 +10,5 @@ df["len_list"] = df["lengths"].apply(lambda  x: list(map(int,x.split(","))))
 df["median"]   = df["len_list"].apply(lambda x: statistics.median(x))
 df["mad"]      = df["len_list"].apply(lambda x: stats.median_abs_deviation(x)   )
 df["size"]     = df["len_list"].apply(lambda x: len(x))
-col_list       = ["median","mad","size"]
+col_list       = ["og_id","median","mad","size"]
 df[col_list].to_csv(out_tsv_file,sep="\t",index=False,header=False)
--- get_og_stats.sh
+++ get_og_stats.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-get_lengths() {
+function get_lengths() {
     fasta_dir=$1
     fasta_file=$2
     base_name=$(echo ${fasta_file} | rev | cut -d\. -f1 --complement | rev)
@@ -7,10 +7,57 @@ get_lengths() {
     echo -e "${base_name}\t${lengths}"
     }
 export -f get_lengths
+function usage(){
+    echo "Utility to calculate the protein median length, median absolute deviation and size of a collection of orthogroups"
+    echo
+    echo "Use this script to get a quick summary of the orthogroups built before and after running OrthoPrep"
+    echo
+    echo "Options:"
+    echo "  --fasta_dir  -> Directory containing protein sequences in fasta format (mandatory)"
+    echo "  --threads    -> Number of CPU threads to use"
+    echo
+    echo "Example:"
+    echo "  get_og_stats.sh --fasta_dir /path/to/my/sequences/folder --threads 16"
+    echo
+	}
+export -f usage
 work_dir=$(pwd)
-fasta_dir=$(realpath $1)
-threads=$2
-fasta_list=$(ls ${fasta_dir} | sort -V | uniq)
+while [ "$1" != "" ]
+do
+    case $1 in
+        --fasta_dir    )
+            shift
+            fasta_dir=$(realpath $1)
+            ;;
+        --threads )
+            shift
+            threads=$1
+            ;;
+		--help         )
+            usage
+            exit 0
+            ;;
+	esac
+	shift
+done
+if [ ! -d "${fasta_dir}" ] || [ -z "${fasta_dir}" ]
+then
+    echo "Missing fasta directory. Exiting"
+    exit 0
+fi
+num_proc=$(nproc)
+if [ -z "${threads}" ]
+then
+    threads=${num_proc}
+else
+    threads_test=$(echo -e "${threads}\t${num_proc}" | awk '{if((int($1)==$1) && ($1<=$2)){print $1}}')
+    if [ -z "${threads_test}" ]
+    then
+        echo "Invalid threads value. Exiting"
+        exit
+    fi
+fi
+fasta_list=$(ls "${fasta_dir}" | sort -V | uniq)
 fasta_count=$(echo "${fasta_list}" | wc -l)
 fasta_iters=$(echo ${fasta_count} | awk '{modulo=$1%1000}{if(modulo==0){print $1/1000}else{print (($1-modulo)/1000)+1 }}')
 for iter in $(seq 1 ${fasta_iters})
